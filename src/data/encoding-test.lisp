5645ae4a377142ce82c701a4892c91cc40778a89
Kilian Sprotte <ksprotte@common-lisp.net>
2008-05-26 15:05:34 +0000

    changed test string.decode-utf-8 to expect flexi-streams:external-format-encoding-error instead of a flexi-streams:*substitution-char* inserted

--- src/data/encoding-test.lisp
+++ src/data/encoding-test.lisp
@@ -116,7 +116,8 @@
                (bknr.datastore::%decode-string in))))    
     (test:is (string-equal "<=>" (decode-string-from-octets #(1 3 60 61 62))))
     ;; #\? is the substitution char
-    (test:is (string-equal "<?>" (decode-string-from-octets #(1 3 60 188 62))))
+    (test:signals flexi-streams:external-format-encoding-error
+      (string-equal "<?>" (decode-string-from-octets #(1 3 60 188 62))))
     ;; kilian 2008-03-20: the following for-all test failed on ccl,
     ;; because the correct utf-8 sequence could produce a char-code
     ;; above char-code-limit - bknr.datastore::%decode-string should
